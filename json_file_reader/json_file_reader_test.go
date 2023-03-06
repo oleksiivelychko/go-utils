611bc7486d9f49782afe0a6e044e68ec0960e044
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-06 09:46:43 +0200

    change error output flow

--- json_file_reader/json_file_reader_test.go
+++ json_file_reader/json_file_reader_test.go
@@ -9,10 +9,10 @@ func TestReadJsonFile(t *testing.T) {
 	bytes, err := ReadJsonFile("./fixtures/data.json")
 
 	if err != nil {
-		t.Fatalf("unable to read json file: %s", err)
+		t.Errorf("unable to read json file: %s", err)
 	}
 	if len(bytes) == 0 {
-		t.Fatal("unable to read data from json file")
+		t.Error("unable to read data from json file")
 	}
 }
 
@@ -20,7 +20,7 @@ func TestReadJsonFileDoesNotExist(t *testing.T) {
 	_, err := ReadJsonFile("./fixtures/data.jsn")
 
 	if err == nil {
-		t.Fatalf("something went wrong, file does not exist")
+		t.Error("something went wrong, file does not exist")
 	}
 
 	fmt.Println(err.Error())
