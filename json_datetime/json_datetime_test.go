633c319e42ec689cf44c866f2838015527145e2e
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-07 08:56:59 +0200

    cover json_datetime package by tests

---
+++ json_datetime/json_datetime_test.go
@@ -0,0 +1,49 @@
+package json_datetime
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+)
+
+type TestStruct struct {
+	DateTime JsonDateTime `json:"dateTime"`
+}
+
+func TestMarshalJsonDateTime(t *testing.T) {
+	testStruct := &TestStruct{DateTime: JsonDateTime{}}
+	dateTimeNowString, err := json.Marshal(testStruct)
+	if err != nil {
+		t.Errorf("unable to marshal json datetime: %s", err)
+	}
+
+	unmarshalStruct := &TestStruct{}
+	_ = json.Unmarshal(dateTimeNowString, &unmarshalStruct)
+
+	if time.Now().Format(time.DateOnly) != unmarshalStruct.DateTime.Format(time.DateOnly) {
+		t.Error("date are not same")
+	}
+}
+
+func TestUnmarshalJsonDateTime(t *testing.T) {
+	dtSample := "2022-10-09T08:23:55.267Z"
+
+	testStruct := &TestStruct{}
+	jsonString := []byte(fmt.Sprintf(`{"dateTime":"%s"`, dtSample))
+
+	err := json.Unmarshal(jsonString, &testStruct)
+	if err != nil {
+		t.Errorf("unable to unmarshal json datetime: %s", err)
+	}
+
+	timeRFC3339, err := time.Parse(time.RFC3339, dtSample)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	marshalStruct := &TestStruct{DateTime: JsonDateTime{timeRFC3339}}
+	if marshalStruct.DateTime.Format(time.RFC3339Nano) != dtSample {
+		t.Error("datetime are not same")
+	}
+}
