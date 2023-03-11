02d77e1ed1ea835784b866bb84276c4e36510363
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-11 10:35:26 +0200

    cover validator package by tests

---
+++ validator/validator_test.go
@@ -0,0 +1,37 @@
+package validator
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"testing"
+)
+
+type TestStruct struct {
+	Count int `json:"count" validate:"required,countValidator"`
+}
+
+func TestValidation(t *testing.T) {
+	v := NewValidation()
+
+	err := v.Validate(TestStruct{Count: 1})
+	if err == nil {
+		t.Error("TestStruct.Count validation failed")
+	}
+
+	err = v.Validate(TestStruct{Count: -1})
+	if err != nil {
+		fmt.Printf("TestStruct.Count validation failed: %s\n", err.Errors())
+	} else {
+		t.Error("TestStruct.Count validation did not invoke")
+	}
+}
+
+func NewValidation() *Validation {
+	newValidator := validator.New()
+	_ = newValidator.RegisterValidation("countValidator", validateCount)
+	return &Validation{newValidator}
+}
+
+func validateCount(field validator.FieldLevel) bool {
+	return field.Field().Int() >= 0
+}
